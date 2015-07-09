57c9f0831e68431dad8413c25a30c997539319ce
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2015-07-09 23:05:30 +0300

    make 1 pdf

--- src/main/java/hello/ScheduledTasks.java
+++ src/main/java/hello/ScheduledTasks.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -13,6 +14,7 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -20,35 +22,42 @@ import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.dom4j.Node;
 import org.dom4j.io.DOMReader;
-import org.dom4j.io.HTMLWriter;
 import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.w3c.tidy.Tidy;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
 @Component
 public class ScheduledTasks {
 	private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-	private String dirName = "/home/roman/jura/html2pdf/OUT/";
-	final Path pathStart = Paths.get(dirName);
+	private static String dirName = "/home/roman/jura/html2pdf/OUT/";
 	private String dirPdfName = "/home/roman/jura/html2pdf/PDF/";
-	private String autoName = "";
+//	private String dirTmpName = "/home/roman/jura/html2pdf/tmp/";
+	final static Path pathStart = Paths.get(dirName);
+	private String autoName = "", h2 = "", h3 = "", h4 = "";
 	private Document autoDocument = null;
+	Element bodyElAutoDocument = null;
 	private	int fileIdx = 0;
 	Tidy tidy = getTidy();
 	DOMReader domReader = new DOMReader();
+	int filesCount;
+	long startMillis;
 
 	@Scheduled(fixedRate = 5000000)
 	public void reportCurrentTime() {
-		int filesCount = countFiles(pathStart.toFile());
+		startMillis = Calendar.getInstance().getTimeInMillis();
+		filesCount = countFiles2(pathStart.toFile());
 		logger.debug("Files count "
 				+ filesCount
-				+ "The time is now " + dateFormat.format(new Date()));
+				+ ". The time is now " + dateFormat.format(new Date()));
 		try {
 			scanFiles();
 		} catch (IOException e) {
@@ -65,63 +74,111 @@ public class ScheduledTasks {
 					BasicFileAttributes attrs) throws IOException {
 				final FileVisitResult visitFile = super.visitFile(file, attrs);
 				fileIdx++;
-				logger.debug(fileIdx + "" + file);
-				if(fileIdx>2){
-					saveHtml(autoDocument, dirPdfName + autoName+ ".html");
+				int procent = fileIdx*100/filesCount;
+				long sec = (Calendar.getInstance().getTimeInMillis() - startMillis)/1000;
+				logger.debug(fileIdx + ""
+						+ "/"
+						+ filesCount
+						+ "("
+						+ procent
+						+ "%, "
+						+ sec
+						+ "s)" + file);
+				if(fileIdx == filesCount){
+					saveHtmlAndPdf(autoDocument, dirPdfName + autoName+ ".html");
 					return null;
 				}
 				final String fileName = file.toString();
-				String[] folders = fileName.replace(dirName, "").replace("\\.html", "").split("\\/");
-				if(!autoName.equals(folders[0]))
-				{
-					if(autoDocument != null)
-					{
-						saveHtml(autoDocument, dirPdfName + autoName+ ".html");
-					}
-					autoDocument = DocumentHelper.createDocument();
-					Element htmElAutoDocument = autoDocument.addElement("html");
-					Element headElAddElement = htmElAutoDocument.addElement("head");
-					addUtf8(headElAddElement);
-					Element bodyElAutoDocument = htmElAutoDocument.addElement("body");
-					autoName = folders[0];
-					//Заголовок документа = ім'я машини
-					//Document head = vehicle name
-					String autoNameHuman = autoName.replace("_", " ");
-					headElAddElement.addElement("title").addText(autoNameHuman);
-					bodyElAutoDocument.addElement("h1").addText(autoNameHuman);
-				}
+				String[] folders = fileName.replace(dirName, "").replace(".html", "").split("\\/");
+				logger.debug(""+folders);
 				final String[] splitFileName = fileName.split("\\.");
 				final String fileExtention = splitFileName[splitFileName.length - 1];
 				if("html".equals(fileExtention)){
-					logger.debug(fileIdx + fileName);
+					if(!autoName.equals(folders[0]))
+					{
+						if(autoDocument != null)
+						{
+							saveHtmlAndPdf(autoDocument, dirPdfName + autoName+ ".html");
+						}
+						autoDocument = DocumentHelper.createDocument();
+						Element htmElAutoDocument = autoDocument.addElement("html");
+						Element headElAddElement = htmElAutoDocument.addElement("head");
+						addUtf8(headElAddElement);
+						bodyElAutoDocument = htmElAutoDocument.addElement("body");
+						autoName = folders[0];
+						//Заголовок документа = ім'я машини
+						//Document head = vehicle name
+						String autoNameHuman = autoName.replace("_", " ");
+						headElAddElement.addElement("title").addText(autoNameHuman);
+						bodyElAutoDocument.addElement("h1").addText(autoNameHuman);
+					}
+					if(!h2.equals(folders[1]))
+					{
+						h2 = folders[1];
+						bodyElAutoDocument.addElement("h2").addText(h2);
+					}
+					if(!h3.equals(folders[2]))
+					{
+						h3 = folders[2];
+						bodyElAutoDocument.addElement("h3").addText(h3);
+					}
+					if(!h4.equals(folders[3]))
+					{
+						h4 = folders[3];
+						bodyElAutoDocument.addElement("h4").addText(h4);
+					}
 					Document document = html2xhtml(file.toFile());
 					document.selectSingleNode("/html/body//p[a/@class='print-page-button']").detach();
 					document.selectSingleNode("/html/body/div/div[@class='back-to-top']").detach();
 					for (Element el : (List<Element>) document.selectNodes("/html/body/div//p/span[contains(text(),'Fig. Fig.')]")) {
 						el.setText(el.getText().replace("Fig. Fig.", "Fig. "));
 					}
-					document.selectSingleNode("/html/head/title").detach();
-					saveHtml(document, dirName +"test"+ fileIdx+ ".html");
+					Node bodyDivEl = document.selectSingleNode("/html/body/div").detach();
+					bodyElAutoDocument.add(bodyDivEl);
+//					document.selectSingleNode("/html/head/title").detach();
+//					saveHtml(document, dirTmpName +"test"+ fileIdx+ ".html");
 				}
 				return visitFile;
 			}
 
-			private void saveHtml(Document document, String htmlOutFileName) {
+			private void saveHtmlAndPdf(Document document, String htmlOutFileName) {
+				System.out.print(htmlOutFileName);
 				Element headEl = (Element) document.selectSingleNode("/html/head");
 				addUtf8(headEl);
 				writeToFile(document, htmlOutFileName);
+				try {
+					savePdf(htmlOutFileName, htmlOutFileName+".pdf");
+				} catch (com.lowagie.text.DocumentException | IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			private void savePdf(String htmlOutFileName, String HTML_TO_PDF) throws com.lowagie.text.DocumentException, IOException {
+				String File_To_Convert = htmlOutFileName;
+				String url = new File(File_To_Convert).toURI().toURL().toString();
+				System.out.println(" 44 "+url);
+				OutputStream os = new FileOutputStream(HTML_TO_PDF);
+				ITextRenderer iTextRenderer2 = new ITextRenderer();
+				ITextRenderer iTextRenderer = iTextRenderer2;
+				ITextRenderer renderer = iTextRenderer;
+				renderer.setDocument(url);
+				renderer.layout();
+				System.out.println(" 49 ");
+				renderer.createPDF(os);
+				System.out.println(" 51 ");
+				os.close();
 			}
 
 			private void addUtf8(Element headEl) {
-				headEl
-				.addElement("meta").addAttribute("charset", "utf-8");
+				headEl.addElement("meta").addAttribute("charset", "utf-8");
 			}
 
 			OutputFormat prettyPrintFormat = OutputFormat.createPrettyPrint();
 			private void writeToFile(Document document, String htmlOutFileName) {
 				try {
 					FileOutputStream fileOutputStream = new FileOutputStream(htmlOutFileName);
-					HTMLWriter xmlWriter = new HTMLWriter(fileOutputStream, prettyPrintFormat);
+//					HTMLWriter xmlWriter = new HTMLWriter(fileOutputStream, prettyPrintFormat);
+					XMLWriter xmlWriter = new XMLWriter(fileOutputStream, prettyPrintFormat);
 					xmlWriter.write(document);
 					xmlWriter.close();
 				} catch (FileNotFoundException e) {
@@ -184,13 +241,13 @@ public class ScheduledTasks {
 		return tidy;
 	}
 
-	public static int countFiles(File directory) {
+	public static int countFiles2(File directory) {
 		int count = 0;
 		for (File file : directory.listFiles()) {
 			if (file.isDirectory()) {
-				count += countFiles(file); 
-			}
-			count++;
+				count += countFiles2(file); 
+			}else
+				count++;
 		}
 		return count;
 	}
